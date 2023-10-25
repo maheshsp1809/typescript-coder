84320b376c5fbcff8e64c1c06ab8cbe5303fb952
maheshsp <maheshsp1809@gmail.com>
2023-10-25 18:53:02 +0530

    added

--- Question.js
+++ Question.js
@@ -1,8 +1,10 @@
 "use strict";
 function swap(a, b) {
-    var c = a;
-    a = b;
-    b = c;
-    return [a, b];
+    return [b, a];
 }
 console.log("ans " + swap("mahesh", "afroz"));
+// Question 2
+function swap2(a, b) {
+    return [b, a];
+}
+console.log("ans " + swap2("mahesh", true));
--- Question.ts
+++ Question.ts
@@ -1,6 +1,14 @@
 type val=string|number|boolean;
 function swap<T>(a: T, b: T):[T,T]{
    return [b,a];
-
+  
 }
 console.log("ans "+swap("mahesh","afroz"));
+
+// Question 2
+
+function swap2<T, U>(a: T, b:U): [U,T] {
+    return [b, a]
+}
+
+console.log("ans " + swap2("mahesh", true));
