135f13dacc76c7df6cf0234b3c9cb581ea70ec3b
maheshsp1809 <maheshsp1809@gmail.com>
2024-02-22 11:17:58 +0530

    ts practice

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,29 @@
 import { Client } from "pg";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const client = new Client({
-  connectionString:
-    "postgresql://lionmahi4571:1riVXmO5dzNT@ep-nameless-shape-a1na6zs1.ap-southeast-1.aws.neon.tech/neondb?sslmode=require",
+  connectionString: process.env.DATABASE_URL
 });
 
 async function createUsersTable() {
-  await client.connect();
-  const result = await client.query(`CREATE TABLE users (
-        id SERIAL PRIMARY KEY,
-        username VARCHAR(50) UNIQUE NOT NULL,
-        email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+  try {
+    await client.connect();
+    const result = await client.query(`CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(50) UNIQUE NOT NULL,
+      email VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
     );`);
-  console.log(result);
-  console.log("Connected successfully");
+    console.log(result);
+    console.log("Table created successfully");
+  } catch (error:any) {
+    console.error('Error:', error.message);
+  } finally {
+    await client.end();
+  }
 }
+
 createUsersTable();
